73ac5e41c07be171d145da7158d6ac0a77a5f2a9
ScarlettTheNeko <scarlett.vd.sluijs@outlook.de>
2025-11-11 11:15:15 +0100

    test

--- Master/bib.js
+++ Master/bib.js
@@ -2,10 +2,11 @@ function renderAktuelles(targetID, elements) {
     const target = document.getElementById(targetID);
 
     const html = elements.map(e => `
-        <div class="aktuelles_element">
+        <article class="aktuelles_element">
         <h3>${e.title}</h3>
+        <p><strong>${e.zeitraum}$</strong></p>
         <p>${e.text}</p>
-        </div>
+        </article>
     `).join("");
 
     target.innerHTML = html;
