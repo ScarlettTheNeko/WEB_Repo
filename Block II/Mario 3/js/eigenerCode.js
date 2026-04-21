ead4929b986a20ef3973d419509b6bc22d2e8697
ScarlettTheNeko <scarlett.vd.sluijs@outlook.de>
2026-04-21 11:19:43 +0200

    m

--- Block II/Mario 3/js/eigenerCode.js
+++ Block II/Mario 3/js/eigenerCode.js
@@ -10,10 +10,13 @@ function main() {
     protName = "Definetiv nicht MArio";
 
     texts = [
+        "Let's go",
         "<b>" + protName + "</b> betritt das Level. Er fühlt sich klein. Er besitzt 0 Goldmünzen. <b>" + protName + "</b> kann nicht nach links gehen... also geht er nach rechts.",
         "Nach einiger Zeit kommt eine fies blickende Kanonenkugel auf <b>" + protName + "</b> zugeflogen."
     ];
     currentIndex = 0;
+
+    buttyHandle();
 }
 
 
@@ -21,9 +24,16 @@ function buttyHandle(){
     schrifty = document.getElementById("gerahmterKasten");
     schrifty.innerHTML = "<p>" + texts[currentIndex] + "</p>";
 
-    if (currentIndex < texts.length - 1) {
+    if (currentIndex == 0) {
+        button = document.getElementById("butty");
+        button.innerHTML = "Abenteuer Starten!";
         currentIndex = currentIndex + 1;
+    } else if (currentIndex < texts.length - 1) {
+        currentIndex = currentIndex + 1;
+        button = document.getElementById("butty");
+        button.innerHTML = "Weiter!";
     } else {
         currentIndex = 0;
+        button.innerHTML = "Zu ende";
     }
 }
