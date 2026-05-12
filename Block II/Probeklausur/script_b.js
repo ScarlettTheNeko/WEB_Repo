f4245c4ab2df3fafcb7cf7778b5c14f79877037d
ScarlettTheNeko <scarlett.vd.sluijs@outlook.de>
2026-05-12 11:21:49 +0200

    meep

--- Block II/Probeklausur/script_b.js
+++ Block II/Probeklausur/script_b.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => main());
 
 let products = [
-    ["p1.webp", "Reithose", "169,99"],
-    ["p2.webp", "Schabracke", "79,99"],
+    ["p1.webp", "Reithose", "169,99", "Eine tolle Reithose ... Wahrscheinlich"],
+    ["schabracke.png", "Schabracke", "69,69", "Wunderschöne Schabracke ... Einfach die beste"],
+    ["OphenaSSilverProductMainGray-BG_720x.webp", "Sicherheitssteigbügel", "339", "Magnetische Sicheritssteigbügel"]
 ];
 
 let currentIndex = 0;
+let guthaben = 1000;
+let summe = 0;
+let istPate = false;
 
 function main(){
     setProduct();
@@ -15,16 +19,24 @@ function main(){
 
     let buttyNext = document.getElementById("nextItem");
     buttyNext.addEventListener("click", () => nextItem());
+
+    let buyButty = document.getElementById("kaufen");
+    buyButty.addEventListener("click", () => buyItem());
+
+    let guthabenH = document.getElementById("Guthaben");
+    guthabenH.innerHTML = "Guthaben: " + guthaben.toFixed(2).replace(".", ",") + " €";
 }
 
 function setProduct(){
     let img = document.getElementById("productPic");
     let name = document.getElementById("productName");
     let price = document.getElementById("productPrice");
+    let desc = document.getElementById("productDesc");
 
     img.src = "img/" + products[currentIndex][0];
     name.innerHTML = products[currentIndex][1];
-    price.innerHTML = products[currentIndex][2];
+    price.innerHTML = products[currentIndex][2] + " €";
+    desc.innerHTML = products[currentIndex][3];
 }
 
 function prevItem(){
@@ -44,4 +56,41 @@ function nextItem(){
         currentIndex++;
     }
     setProduct();
+}
+
+function getPreis(index) {
+    return parseFloat(products[index][2].replace(",", "."));
+}
+
+
+function checkGuthaben() {
+    return guthaben >= getPreis(currentIndex);
+}
+
+function buyItem() {
+    if (!checkGuthaben()) {
+        window.alert("Guthaben nicht ausreichend!!!");
+        return;
+    }
+
+    let readableTime = new Date().toLocaleString("de-DE");
+
+    let productName = products[currentIndex][1];
+    let price = getPreis(currentIndex);
+
+    let table = document.getElementById("warenkorbTabelle");
+    let row = table.insertRow(1);
+
+    row.insertCell(0).innerHTML = readableTime;
+    row.insertCell(1).innerHTML = productName;
+    row.insertCell(2).innerHTML = price.toFixed(2).replace(".", ",") + " €";
+
+    summe += price;
+    guthaben -= price;
+
+    document.getElementById("summeSign").innerHTML =
+        "Summe: " + summe.toFixed(2).replace(".", ",") + " €";
+
+    document.getElementById("Guthaben").innerHTML =
+        "Guthaben: " + guthaben.toFixed(2).replace(".", ",") + " €";
 }
